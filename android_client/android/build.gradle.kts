0383da6b3e7b51c86add95472977b91bf1e98bd4
epicnellson <nelsonemmanuel006@gmail.com>
2026-07-01 11:59:25 +0100

    fix: set namespace for library plugins that lack it (AGP 9.x compat)

--- android_client/android/build.gradle.kts
+++ android_client/android/build.gradle.kts
@@ -15,8 +15,21 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    plugins.whenPluginAdded {
+        if (hasPlugin("com.android.library") && !hasPlugin("com.android.application")) {
+            extensions.findByType<com.android.build.gradle.LibraryExtension>()?.let { ext ->
+                if (ext.namespace == null) {
+                    ext.namespace = "com.example.${project.name.replace("-", "_")}"
+                }
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
